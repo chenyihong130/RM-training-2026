bd8989673d5e1b39a5b0f98c05e487dab845c407
chenyihong130 <239976774@qq.com>
2026-05-16 16:39:38 +0800

    w2's work

--- w2/ros2_test/src/training_pkg/src/talker.cpp
+++ w2/ros2_test/src/training_pkg/src/talker.cpp
@@ -10,6 +10,12 @@ class Talker : public rclcpp::Node
 public:
     Talker() : Node("talker_node"), count_(0)
     {
+        price_desc.description = "话题价格参数，取值范围1~1000，默认值100";
+        price_desc.integer_range.resize(1);
+        price_desc.integer_range[0].from_value = 1;
+        price_desc.integer_range[0].to_value = 1000;
+        price_desc.integer_range[0].step = 1;
+        this->declare_parameter("price", price, price_desc);
         publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
         timer_ = this->create_wall_timer(
             500ms, std::bind(&Talker::timer_callback, this));
@@ -22,11 +28,15 @@ private:
         message.data = "Hello, ROS2! Count: " + std::to_string(count_++);
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
         publisher_->publish(message);
+        this->get_parameter("price", price);
+        RCLCPP_INFO(this->get_logger(), "Current price: %d", price);
     }
     
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     size_t count_;
+    int price;
+    rcl_interfaces::msg::ParameterDescriptor price_desc;
 };
 
 int main(int argc, char **argv)
